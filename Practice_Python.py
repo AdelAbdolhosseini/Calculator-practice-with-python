f9ae785a210c40c701d39a33d0abe8a6c5615f13
Adel <Adelabdolhosseini@gmail.com>
2022-04-19 14:20:04 +0430

    create git

---
+++ Practice_Python.py
@@ -0,0 +1,16 @@
+intrudoction = "adel abdolhosseini"
+age = 39
+son = "ali"
+name = input("Please insert your name? ")
+print("Hello " + name)
+introdce = input("please introduce your self? ")
+print(introdce + " I am " + intrudoction.capitalize() + " I am " + str(age) + " years old " + "I have a son his name is " + son.capitalize())
+old = input("How ols is he? ")
+print("He is " + old)
+
+
+
+
+
+
+
