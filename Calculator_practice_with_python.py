e85b0cefc694542b2c7d62fe125985c4cf0e8277
Adelabdolhosseini <Adelabdolhosseini@gmail.com>
2022-04-20 10:03:48 +0430

    Update project

--- Calculator_practice_with_python.py
+++ Calculator_practice_with_python.py
@@ -1,14 +1,23 @@
+import math
+
 number1 = input("Please insert the first number: ")
 number2 = input("Please insert the second number: ")
-operation = input("Please insert desired operation among: + , - , * , / :  ")
+operation = input("Please insert desired operation among: + , - , * , / , %, **, math.sqrt: ")
 if operation == "+":
     result = float(number1) + float(number2)
-if operation == "-":
-    result = float(number1) - float(number2)
-if operation == "*":
+elif operation == "+":
+    result = float(number1) + float(number2)
+elif operation == "*":
     result = float(number1) * float(number2)
-if operation == "/":
+elif operation == "/":
     result = float(number1) / float(number2)
+elif operation == "%":
+    result = float(number1) % float(number2)
+elif operation == "**":
+    result = float(number1) ** float(number2)
+
+else:
+    result = "not exist"
 
 print(result)
 
