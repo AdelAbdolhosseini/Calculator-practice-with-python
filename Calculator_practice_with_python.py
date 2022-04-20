427f16a2f6826448ef2b47bf9980f29fefddc0f0
Adelabdolhosseini <Adelabdolhosseini@gmail.com>
2022-04-20 10:10:57 +0430

    change file

--- Calculator_practice_with_python.py
+++ Calculator_practice_with_python.py
@@ -1,6 +1,6 @@
 number1 = input("Please insert the first number: ")
 number2 = input("Please insert the second number: ")
-operation = input("Please insert desired operation among: + , - , * , / , %, **, math.sqrt: ")
+operation = input("Please insert desired operation among: + , - , * , / , %, **: ")
 if operation == "+":
     result = float(number1) + float(number2)
 elif operation == "+":
